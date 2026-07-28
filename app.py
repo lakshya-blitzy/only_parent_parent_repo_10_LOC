d19b79863b2a0556b66e79230dc4955aacc1bcbc
lakshya-blitzy <lakshya@blitzy.com>
2026-07-28 13:28:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+def greet(name):
+    return f"Hello {name}"
+
+if __name__ == "__main__":
+    user = "Lakshya"
+    print(greet(user))
+  if __name__ == "__main__":
+    user = "asdasdafsad"
+    print(greet(user))
+    ///asdas
